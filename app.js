7a67a6785de7898ede551962b296e9347b7ec7bf
Nguyễn Trần Thắng <51274694+tranthang9111997@users.noreply.github.com>
2022-04-23 06:47:25 +0700

    pc

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ connect();
 //     next(createError(404));
 // });
 function connect() {
-    var db = "mongodb://localhost:27017/sockserver";
+    var db = "mongodb://127.0.0.1:27017/sockserver";
     mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true });
     const connection = mongoose.connection;
     connection.once("open", function () {
